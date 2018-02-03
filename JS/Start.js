9e24765b0f97665a06701974d5bdd51e3c56fa9e
Chris Jackson <shadowace248@gmail.com>
2018-02-03 13:55:58 -0700

    Checkbox to Button

--- JS/Start.js
+++ JS/Start.js
@@ -41,168 +41,182 @@ var roundedTempLeft;
 var roundedTempRight;
 
 $(function(){
-	var canvas = document.getElementById('mainCanvas');
-	var c = canvas.getContext('2d');
-	ctx = c;
-	printInputs();
-	init(c);
-	refreshSize();
+    var canvas = document.getElementById('mainCanvas');
+    var c = canvas.getContext('2d');
+    ctx = c;
+    printInputs();
+    init(c);
+    refreshSize();
 });
 
 function resetLoop(){
-	//allowCrossOver = false;
-	if(allowCrossOver){
-		$('[type=checkbox]').click();
-	}
-	stopwatchTime = 0;
-	$('#time').html(0);
-	clearInterval(gameLoop);
-	init(ctx);
+    //allowCrossOver = false;
+    if(allowCrossOver){
+        $('[type=checkbox]').click();
+    }
+    stopwatchTime = 0;
+    $('#time').html(0);
+    clearInterval(gameLoop);
+    init(ctx);
 }
 
 function init(c) {
-	var particlesLeft = [];
-	var particlesRight = [];
-	
-	//premake particle obj
-	for (let i = 0; i < numberOfParticlesLeft; i++) {
-		let initForce = new Point(0, 0);
-		initForce.r = initTempLeft;
-		initForce.deg = i * (360 / numberOfParticlesLeft);
-		let initPoint = getPointOnCircle(((2 * numberOfParticlesLeft - 1) * particleRadius - 1) / (2 * Math.PI), i * (1 / numberOfParticlesLeft))
-		initPoint.x += 50;
-		initPoint.y += 50;
+    var particlesLeft = [];
+    var particlesRight = [];
+    
+    //premake particle obj
+    for (let i = 0; i < numberOfParticlesLeft; i++) {
+        let initForce = new Point(0, 0);
+        initForce.r = initTempLeft;
+        initForce.deg = i * (360 / numberOfParticlesLeft);
+        let initPoint = getPointOnCircle(((2 * numberOfParticlesLeft - 1) * particleRadius - 1) / (2 * Math.PI), i * (1 / numberOfParticlesLeft))
+        initPoint.x += 50;
+        initPoint.y += 50;
 
-		makeParticle(initPoint, particleRadius, particleMass, initForce, particlesLeft);
-	}
-	
-	for (let i = 0; i < numberOfParticlesRight; i++) {
-		let initForce = new Point(0, 0);
-		initForce.r = initTempRight;
-		initForce.deg = i * (360 / numberOfParticlesRight);
-		let initPoint = getPointOnCircle(((2 * numberOfParticlesRight - 1) * particleRadius - 1) / (2 * Math.PI), i * (1 / numberOfParticlesRight))
-		initPoint.x += 151;
-		initPoint.y += 50;
+        makeParticle(initPoint, particleRadius, particleMass, initForce, particlesLeft);
+    }
+    
+    for (let i = 0; i < numberOfParticlesRight; i++) {
+        let initForce = new Point(0, 0);
+        initForce.r = initTempRight;
+        initForce.deg = i * (360 / numberOfParticlesRight);
+        let initPoint = getPointOnCircle(((2 * numberOfParticlesRight - 1) * particleRadius - 1) / (2 * Math.PI), i * (1 / numberOfParticlesRight))
+        initPoint.x += 151;
+        initPoint.y += 50;
 
-		makeParticle(initPoint, particleRadius, particleMass, initForce, particlesRight);
-	}
+        makeParticle(initPoint, particleRadius, particleMass, initForce, particlesRight);
+    }
 
-	//initArrowKeys();
+    //initArrowKeys();
 
-	//start loop
-	gameLoop = setInterval(makeMainLoop(particlesLeft, particlesRight, 100), 1000 / fps);
+    //start loop
+    gameLoop = setInterval(makeMainLoop(particlesLeft, particlesRight, 100), 1000 / fps);
 }
 window.onresize = refreshSize;
 
+function toggleBarrier(){
+    let $button = $('#barrierButton');
+    $button.toggleClass('on');
+    allowCrossOver = $button.hasClass('on');
+    setBarrierButtonText();
+}
+
+function setBarrierButtonText(){
+    if(allowCrossOver){
+        $('#barrierButton').text("Add Barrier");
+    }else{
+        $('#barrierButton').text("Remove Barrier");
+    }
+}
+
 function makeParticle(initPoint, radius, mass, initForce, list) {
-	var particle = new Point(initPoint.x, initPoint.y);
-	particle.vel = new Point(0, 0);
-	particle.mass = mass;
-	particle.radius = radius;
-	particle.applyForce = function (force) {
-		accelleration = force.scale(1 / particle.mass);
-		particle.vel.x += accelleration.x;
-		particle.vel.y += accelleration.y;
-	}
-	particle.applyForce(initForce);
-	list.push(particle);
+    var particle = new Point(initPoint.x, initPoint.y);
+    particle.vel = new Point(0, 0);
+    particle.mass = mass;
+    particle.radius = radius;
+    particle.applyForce = function (force) {
+        accelleration = force.scale(1 / particle.mass);
+        particle.vel.x += accelleration.x;
+        particle.vel.y += accelleration.y;
+    }
+    particle.applyForce(initForce);
+    list.push(particle);
 }
 
 function printInputs() {
-	settingContainers = $('.setting');
-	settingContainers.each(function (i, settingContainer) {
-		var settingContainer = $(settingContainer);
-		var type = settingContainer.attr('type');
-		var varName = settingContainer.attr('var');
+    setBarrierButtonText();
+    settingContainers = $('.setting');
+    settingContainers.each(function (i, settingContainer) {
+        var settingContainer = $(settingContainer);
+        var type = settingContainer.attr('type');
+        var varName = settingContainer.attr('var');
 
-		var settingInput = $('<input/>');
-		settingInput.attr('type', type);
-		if (type == 'range') {
-			var max = settingContainer.attr('max');
-			var min = settingContainer.attr('min');
-			settingInput.attr('max', max);
-			settingInput.attr('min', min);
-		}
+        var settingInput = $('<input/>');
+        settingInput.attr('type', type);
+        if (type == 'range') {
+            var max = settingContainer.attr('max');
+            var min = settingContainer.attr('min');
+            settingInput.attr('max', max);
+            settingInput.attr('min', min);
+        }
 
-		settingInput.change(function () {
-			var self = $(this);
-			var cont = self.parent();
-			var varName = cont.attr('var');
-			var value;
-			if (self.attr('type') == 'checkbox') {
-				value = self.prop('checked');
-			} else {
-				value = self.val();
-			}
-			if (self.attr('type') == 'range') {
-				value = Number(value);
-			}
-			window[varName] = value;
-		});
-		
-		let alias = settingContainer.attr('alias');
-		settingContainer.append(alias + ': ');
-		settingContainer.append(settingInput);
-		var initValue = window[varName];
-		if (settingInput.attr('type') == 'range') {
-			settingContainer.append('<p>'+initValue+'</p>');
-		}
-		if (settingInput.attr('type') == 'checkbox') {
-			settingInput.attr('checked', initValue);
-		} else {
-			settingInput.val(initValue);
-		}
-		if(settingInput.attr('type')=='range'){
-			settingContainer.append($('<button class="adder">+</buttom>'));
-			settingContainer.append($('<button class="subtractor">-</buttom>'));
-		}
-	});
-	$('[type="range"]').change(function(e){
-		let target = $(this);
-		target.parent().children('p').html(target.val())
-	});
-	$('.adder').click(function(e){
-		let target = $(this);
-		let varName = target.parent().attr('var');
-		let value = window[varName];
-		value++;
-		window[varName] = value;
-		target.parent().children('input').val(value);
-		target.parent().children('p').html(value);
-	});
-	$('.subtractor').click(function(e){
-		let target = $(this);
-		let varName = target.parent().attr('var');
-		let value = window[varName];
-		value--;
-		window[varName] = value;
-		target.parent().children('input').val(value);
-		target.parent().children('p').html(value);
-	});
-	//Clock is http://flipclockjs.com/
-	
+        settingInput.change(function () {
+            var self = $(this);
+            var cont = self.parent();
+            var varName = cont.attr('var');
+            var value;
+            if (self.attr('type') == 'checkbox') {
+                value = self.prop('checked');
+            } else {
+                value = self.val();
+            }
+            if (self.attr('type') == 'range') {
+                value = Number(value);
+            }
+            window[varName] = value;
+        });
+        
+        let alias = settingContainer.attr('alias');
+        settingContainer.append(alias + ': ');
+        settingContainer.append(settingInput);
+        var initValue = window[varName];
+        if (settingInput.attr('type') == 'range') {
+            settingContainer.append('<p>'+initValue+'</p>');
+        }
+        if (settingInput.attr('type') == 'checkbox') {
+            settingInput.attr('checked', initValue);
+        } else {
+            settingInput.val(initValue);
+        }
+        if(settingInput.attr('type')=='range'){
+            settingContainer.append($('<button class="adder">+</buttom>'));
+            settingContainer.append($('<button class="subtractor">-</buttom>'));
+        }
+    });
+    $('[type="range"]').change(function(e){
+        let target = $(this);
+        target.parent().children('p').html(target.val())
+    });
+    $('.adder').click(function(e){
+        let target = $(this);
+        let varName = target.parent().attr('var');
+        let value = window[varName];
+        value++;
+        window[varName] = value;
+        target.parent().children('input').val(value);
+        target.parent().children('p').html(value);
+    });
+    $('.subtractor').click(function(e){
+        let target = $(this);
+        let varName = target.parent().attr('var');
+        let value = window[varName];
+        value--;
+        window[varName] = value;
+        target.parent().children('input').val(value);
+        target.parent().children('p').html(value);
+    });
 }
 
 function setStopwatch(){
-	if(!allowCrossOver){
-		isStopwatchRunning = true;
-	}else{
-		isStopwatchRunning = false;
-	}
+    if(!allowCrossOver){
+        isStopwatchRunning = true;
+    }else{
+        isStopwatchRunning = false;
+    }
 }
 
 function refreshSize() {
-	ctx.canvas.width = window.innerWidth*canvasSize;
-	ctx.canvas.height = window.innerWidth*canvasSize / canvasRatio;
-	$('#canvasContainer').css('width', ctx.canvas.width);
-	$('#canvasContainer label').css('width', ctx.canvas.width/2.1);
-	$('#canvasContainer label').css('display', 'inline-block');
-	$('#canvasContainer label').css('text-align', 'center');
+    ctx.canvas.width = window.innerWidth*canvasSize;
+    ctx.canvas.height = window.innerWidth*canvasSize / canvasRatio;
+    $('#canvasContainer').css('width', ctx.canvas.width);
+    $('#canvasContainer label').css('width', ctx.canvas.width/2.1);
+    $('#canvasContainer label').css('display', 'inline-block');
+    $('#canvasContainer label').css('text-align', 'center');
 }
 
 function getPointOnCircle(radius, time) {
-	var point = new Point(1, 1);
-	point.r = radius;
-	point.rad = 2 * Math.PI * time
-	return point;
+    var point = new Point(1, 1);
+    point.r = radius;
+    point.rad = 2 * Math.PI * time
+    return point;
 }
